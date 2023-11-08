310446d51f5a350d6113407a06a19bb9776b3b6e
Shivanshu <112751483+TheTagEnd@users.noreply.github.com>
2023-11-08 14:17:43 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.css';
+import  TwitterIcon from '@material-ui/icons/Twitter'
+function Sidebar() {
+  return (
+    <div className="sidebar">
+ <TwitterIcon/>
+
+    </div>
+  )
+}
+
+export default Sidebar
